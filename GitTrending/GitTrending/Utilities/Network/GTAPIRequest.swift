1d4516c211be9961323925f7d072309efea9bca6
Vinothini Prasad <vinothiniprasad@gmail.com>
2019-07-08 19:38:02 +0530

    #refs, Added SwiftSoup pod files

---
+++ GitTrending/GitTrending/Utilities/Network/GTAPIRequest.swift
@@ -0,0 +1,9 @@
+//
+//  GTAPIRequest.swift
+//  GitTrending
+//
+//  Created by Bala Vino on 08/07/19.
+//  Copyright © 2019 tarento. All rights reserved.
+//
+
+import Foundation
---
+++ GitTrending/GitTrending/Utilities/Network/GTAPIResponse.swift
@@ -0,0 +1,9 @@
+//
+//  GTAPIResponse.swift
+//  GitTrending
+//
+//  Created by Bala Vino on 08/07/19.
+//  Copyright © 2019 tarento. All rights reserved.
+//
+
+import Foundation
